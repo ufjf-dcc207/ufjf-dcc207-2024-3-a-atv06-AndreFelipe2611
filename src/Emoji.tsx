80848a053e31b0cf2ea0b04a97d18ed1d4e03620
André Felipe <andre1104mg@gmail.com>
2025-01-07 11:07:28 -0300

    ciclo finalizado

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -1,56 +1,56 @@
-import { useState } from 'react';
-import './Emoji.css'
+import { useState } from "react";
+import "./Emoji.css";
 
 const EMOJIS = new Map<string, string>([
-    ["happy", "😎"],
-    ["sick", "🤢"],
-    ["dead", "💀"],
-])
+  ["happy", "😎"],
+  ["sick", "🤢"],
+  ["dead", "💀"],
+]);
 
+export default function Emoji() {
+  const [situacao, setSituacao] = useState("happy");
 
-export default function Emoji(){
-    const [situacao, setSituacao] = useState("happy");
+  function toHappy() {
+    console.log("toHappy()!");
+    setSituacao("happy");
+  }
 
-    function toHappy(){
-        console.log("toHappy()!");
+  function toSick() {
+    console.log("toSick()!");
+    setSituacao("sick");
+  }
+
+  function toDead() {
+    console.log("toDead()!");
+    setSituacao("dead");
+  }
+
+  function cicle() {
+    console.log("cicle()!");
+    switch (situacao) {
+      case "happy":
+        setSituacao("sick");
+        break;
+      case "sick":
+        setSituacao("dead");
+        break;
+      case "dead":
+        setSituacao("happy");
+        break;
+      default:
         setSituacao("happy");
     }
+  }
 
-    function toSick(){
-        console.log("toSick()!");
-        setSituacao("sick");
-    }
-
-    function toDead(){
-        console.log("toDead()!");
-        setSituacao("dead");
-    }
-
-    function cicle(){
-        console.log("cicle()!");
-        switch(situacao){
-            case "happy":
-            setSituacao("sick");
-            break;
-            case "sick":
-            setSituacao("dead");
-            break;
-            case "dead":
-            setSituacao("happy");
-            break;
-            default:
-            setSituacao("happy");
-        }
-    }
-
-    return(
-        <div className="emoji">
-        <div className='situacao'>{EMOJIS.get(situacao) || "🫥"}</div>
-        <div className="acoes">
-            <button onClick={toHappy}>Vivo</button>
-            <button onClick={toSick}>Doente</button>
-            <button onClick={toDead}>Morto</button>
-        </div>
-        </div>
-    ); 
+  return (
+    <div className="emoji">
+      <div className="situacao">{EMOJIS.get(situacao) || "🫥"}</div>
+      <div className="acoes">
+        <button onClick={toHappy}>Vivo</button>
+        <button onClick={toSick}>Doente</button>
+        <button onClick={toDead}>Morto</button>
+        <button onClick={cicle}>Ciclo da vida</button>
+      </div>
+    </div>
+  );
 }
