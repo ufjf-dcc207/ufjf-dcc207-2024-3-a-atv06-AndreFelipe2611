ff9d279d9c5c679a43b8c21a63114e723c09b86b
André Felipe <andre1104mg@gmail.com>
2025-01-09 22:01:50 -0300

    adiciona limites

--- src/Atributo.tsx
+++ src/Atributo.tsx
@@ -1,2 +1,17 @@
+import { useState } from "react";
 import "./Atributo.css";
 
+export default function Status(){
+   const [valor, setValor] = useState(3);
+
+   if(valor === 5){
+    setValor(0);
+   }else{
+    setValor(valor + 1);
+   }
+   
+   return <div className="status">
+        <span>{valor}</span>
+        <button>+</button>
+    </div>
+}
